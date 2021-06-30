caacb87bc1445e0810e6e6014a981cd0046a897b
Robert Bagott <robbagott@gmail.com>
2021-06-30 18:37:58 -0500

    Implemented a simple chrome extension which translates characters every second on a timer.

---
+++ src/autorun.js
@@ -0,0 +1,34 @@
+import Chinese from 'chinese-s2t';
+
+(() => {
+  setInterval(replaceSimplifiedChars, 1000);	
+  
+  function replaceSimplifiedChars() {
+    const wordBankButtons = document.querySelectorAll('[data-test="challenge-tap-token"]');
+
+    wordBankButtons.forEach((node) => {
+      node.innerHTML = Chinese.s2t(node.innerHTML);
+    });
+
+    const wordBankCards = document.querySelectorAll('[data-test="challenge-choice-card"]');
+
+    wordBankCards.forEach((node) => {
+      getLeafNodes(node).forEach((leaf) => {
+        leaf.innerHTML = Chinese.s2t(leaf.innerHTML);
+      });
+    });
+  }
+
+  function getLeafNodes(node) {
+    let leaves = [];
+    if (node.children.length) {
+      for (let i = 0; i < node.children.length; i++) {
+        leaves = leaves.concat(getLeafNodes(node.children[i]));
+      }
+    } else {
+      // Node is a leaf.
+      return [node];
+    }
+    return leaves;
+  }
+})();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'src', 'autorun.js')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "manifest.json"),
+          to: path.resolve(__dirname, "dist")
+        }
+      ]
+    })
+  ]
+};
