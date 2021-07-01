82ace57da851ef7510df831b6f9bc1bcdd334d41
Robert Bagott <robbagott@gmail.com>
2021-07-01 09:41:19 -0500

    Updated manifest and background so that the script is only injected on page load once.

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tabId }, 
-    files: ["main.js"] 
-  });
+  console.log(changeInfo);
+  if (changeInfo.status === 'loading' && tab.url.includes('.duolingo.com')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId }, 
+      files: ["main.js"] 
+    });
+  }
 });
