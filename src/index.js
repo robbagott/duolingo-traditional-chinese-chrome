c2409e6e6619fdfda59d7d209a002cc0cd9041b1
Robert Bagott <robbagott@gmail.com>
2021-07-01 01:25:55 -0500

    Removed async/await syntax due to issues with babel polyfill and multiple injections.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
-import "@babel/polyfill";
 import Chinese from 'chinese-s2t';
 
 // Don't run extension if it's already running. The extension must be injected by background service
 // when the user click the back button in the browser.
+console.log("Injected. Am I running?: ", window.duolingoTraditionalChineseExtensionRunning);
 if (!window.duolingoTraditionalChineseExtensionRunning) {
   window.duolingoTraditionalChineseExtensionRunning = true;
   runExtension();
 }
 
-async function runExtension() {
+function runExtension() {
   // Run immediately before observing.
-  const challengeDiv = await awaitChallengeDiv(); 
-  const challengeParent = challengeDiv.parentNode.parentNode;
-  replaceSimplifiedChars(challengeParent);
-
-  resetMutationObserver();
-
-  async function resetMutationObserver() {
-    const challengeDiv = await awaitChallengeDiv(); 
+  awaitChallengeDiv().then(challengeDiv => {
     const challengeParent = challengeDiv.parentNode.parentNode;
+    replaceSimplifiedChars(challengeParent);
 
-    const observer = new MutationObserver(async (mutations, observer) => {
-      const nodesAdded = mutations.some(mutation => mutation.addedNodes.length > 0);
-      if (nodesAdded) {
-        observer.disconnect();
-        observer.takeRecords();
-        replaceSimplifiedChars(challengeParent);
-        observe(observer, challengeParent);
-      }
-    });
+    resetMutationObserver();
+  }); 
 
-    observe(observer, challengeParent);
+  function resetMutationObserver() {
+    console.log('resetting observer');
+    const challengeDiv = awaitChallengeDiv().then(challengeDiv => {
+      const challengeParent = challengeDiv.parentNode.parentNode;
+
+      const observer = new MutationObserver((mutations, observer) => {
+        const nodesAdded = mutations.some(mutation => mutation.addedNodes.length > 0);
+        if (nodesAdded) {
+          observer.disconnect();
+          observer.takeRecords();
+          replaceSimplifiedChars(challengeParent);
+          observe(observer, challengeParent);
+        }
+      });
+
+      observe(observer, challengeParent);
+      monitorObserverHealth(challengeParent).catch(err => runExtension());
+    }); 
   }
 
   function observe(observer, node) {
@@ -40,19 +44,21 @@ async function runExtension() {
     });
   }
 
-  async function awaitChallengeDiv() {
+  function awaitChallengeDiv() {
     return new Promise(resolve => {
       const challengeDiv = getChallengeDiv();
       if (challengeDiv) {
+        console.log('waiting for challenge...')
         resolve(challengeDiv);
       } else {
         let interval = setInterval(() => {
+        console.log('waiting for challenge...')
           const challengeDiv = getChallengeDiv();
           if (challengeDiv) {
             clearInterval(interval);
             resolve(challengeDiv);
           }
-        }, 10);
+        }, 100);
       }
     });
   }
@@ -69,6 +75,18 @@ async function runExtension() {
       if (node.children.length === 0) {
         node.innerHTML = Chinese.s2t(node.innerHTML);
       }
+    });
+  }
+
+  function monitorObserverHealth(node) {
+    return new Promise((resolve, reject) => {
+      let interval = setInterval(() => {
+        console.log('monitoring observer...');
+        if (!document.body.contains(node)) {
+          clearInterval(interval);
+          reject();
+        }
+      }, 1000);
     });
   }
 };
