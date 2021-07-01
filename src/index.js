419c0b19fb20c31da1da894a954c1689a3b88f44
Robert Bagott <robbagott@gmail.com>
2021-06-30 19:48:52 -0500

    Simplified logic to just look at all divs and spans without children.

--- src/index.js
+++ src/index.js
@@ -4,39 +4,12 @@ import Chinese from 'chinese-s2t';
   setInterval(replaceSimplifiedChars, 1000);	
   
   function replaceSimplifiedChars() {
-    const wordBankButtons = document.querySelectorAll('[data-test="challenge-tap-token"]');
+    const spans = document.querySelectorAll('span, div');
 
-    wordBankButtons.forEach((node) => {
-      node.innerHTML = Chinese.s2t(node.innerHTML);
-    });
-
-    const wordBankCards = document.querySelectorAll('[data-test="challenge-choice-card"]');
-
-    wordBankCards.forEach((node) => {
-      getLeafNodes(node).forEach((leaf) => {
-        leaf.innerHTML = Chinese.s2t(leaf.innerHTML);
-      });
-    });
-
-    const prompts = document.querySelectorAll('[data-test="challenge-translate-prompt"]');
-
-    prompts.forEach((node) => {
-      getLeafNodes(node).forEach((leaf) => {
-        leaf.innerHTML = Chinese.s2t(leaf.innerHTML);
-      });
-    });
-  }
-
-  function getLeafNodes(node) {
-    let leaves = [];
-    if (node.children.length) {
-      for (let i = 0; i < node.children.length; i++) {
-        leaves = leaves.concat(getLeafNodes(node.children[i]));
+    spans.forEach((node) => {
+      if (node.children.length === 0) {
+        node.innerHTML = Chinese.s2t(node.innerHTML);
       }
-    } else {
-      // Node is a leaf.
-      return [node];
-    }
-    return leaves;
+    });
   }
 })();
