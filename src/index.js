713a876f80e9e713d03d6d6fa2e0a06147da7ae1
Robert Bagott <robbagott@gmail.com>
2021-06-30 22:11:35 -0500

    Implemented solution using MutationObserver. Simplified characters no longer blink in and out.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
+import "@babel/polyfill";
 import Chinese from 'chinese-s2t';
 
-(() => {
-  setInterval(replaceSimplifiedChars, 1000);	
-  
-  function replaceSimplifiedChars() {
-    const spans = document.querySelectorAll('span, div');
+(async () => {
+  resetMutationObserver();
 
+  async function resetMutationObserver() {
+    const challengeDiv = await awaitChallenge(); 
+    const challengeParent = challengeDiv.parentNode.parentNode;
+
+    const observer = new MutationObserver(async (mutations, observer) => {
+      const nodesAdded = mutations.some(mutation => mutation.addedNodes.length > 0);
+      if (nodesAdded) {
+        observer.disconnect();
+        observer.takeRecords();
+        replaceSimplifiedChars(challengeParent);
+        observe(observer, challengeParent);
+      }
+    });
+
+    observe(observer, challengeParent);
+  }
+
+  function observe(observer, node) {
+    observer.observe(node, {
+      subtree: true,
+      childList: true,
+    });
+  }
+
+  async function awaitChallenge() {
+    return new Promise(resolve => {
+      const challengeDiv = getChallenge();
+      if (challengeDiv) {
+        resolve(challengeDiv);
+      } else {
+        let interval = setInterval(() => {
+          const challengeDiv = getChallenge();
+          if (challengeDiv) {
+            clearInterval(interval);
+            resolve(challengeDiv);
+          }
+        }, 10);
+      }
+    });
+  }
+
+  function getChallenge() {
+    const challengeQuery = document.querySelectorAll('[data-test~=challenge]');
+    const challengeDiv = challengeQuery.length ? challengeQuery[0] : null; 
+    return challengeDiv;
+  }
+
+  function replaceSimplifiedChars(node) {
+    const spans = node.querySelectorAll('span, div');
     spans.forEach((node) => {
       if (node.children.length === 0) {
         node.innerHTML = Chinese.s2t(node.innerHTML);
